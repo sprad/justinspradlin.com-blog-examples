d7f4f4269463a7551b39c8d9ab9b78c535a296bd
Justin Spradlin <jspradlin@gmail.com>
2010-01-11 22:18:41 -0500

    jQuery and Flickr API code.

---
+++ 2010-01-12_Creating-a-Photostream-Using-jQuery-and-the-Flickr-API/js/custom.js
@@ -0,0 +1,34 @@
+$(function() {
+	var apiKey = '<YOUR FLICKR API KEY>';
+	var userId = '<FLICKR USER ID>';
+	var tag = '<COMMA SEPERATED LIST OF TAGS>';
+	var perPage = '25';
+	var showOnPage = '6';
+	
+	$.getJSON('http://api.flickr.com/services/rest/?format=json&method='+
+		'flickr.photos.search&api_key=' + apiKey + '&user_id=' + userId + 
+		'&tags=' + tag + '&per_page=' + perPage + '&jsoncallback=?', 
+	function(data){
+    	var classShown = 'class="lightbox"';
+		var classHidden = 'class="lightbox hidden"';
+		
+		$.each(data.photos.photo, function(i, rPhoto){
+    		var basePhotoURL = 'http://farm' + rPhoto.farm + '.static.flickr.com/'
+    			+ rPhoto.server + '/' + rPhoto.id + '_' + rPhoto.secret;    		
+    		
+    		var thumbPhotoURL = basePhotoURL + '_s.jpg';
+    		var mediumPhotoURL = basePhotoURL + '.jpg';					    			    		
+    		
+    		var photoStringStart = '<a ';
+    		var photoStringEnd = 'title="' + rPhoto.title + '" href="'+ 
+    			mediumPhotoURL +'"><img src="' + thumbPhotoURL + '" alt="' + 
+    			rPhoto.title + '"/></a>;'    			
+    		var photoString = (i < showOnPage) ? 
+    			photoStringStart + classShown + photoStringEnd : 
+    			photoStringStart + classHidden + photoStringEnd;
+    		    			    		
+    		$(photoString).appendTo("#flickr");
+		});
+		$("a.lightbox").lightBox();
+	});
+});
---
+++ README.txt
@@ -0,0 +1,1 @@
+Code examples from www.justinspradlin.com
